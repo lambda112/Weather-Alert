8b0d03f729e32586137747067d590dd9e60dd8f7
Kamran Azim <Kamazim121212@gmail.com>
2024-08-03 01:04:23 +0100

    Sent message using whatsapp

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests as r
+import os
+from twilio.rest import Client
 
 # Get Information From Website
-api_key = "65bed5c069ccfb53c44648160e7bc41b"
+api_key = "c6ba93a799a5f2fca3a6b1f2e232520b"
 response = r.get("https://api.openweathermap.org/data/2.5/forecast", params={"lat":53.537980, "lon":-2.092610, "appid": api_key, "cnt":7})
 response.raise_for_status()
 
@@ -12,6 +14,7 @@ weather_dict = {}
 for index, data in enumerate(weather_data):
     weather_dict[index] = data
 
+weather_information = []
 for key, val in weather_dict.items():
     info = val["weather"][0]
     time = val["dt_txt"]
@@ -20,4 +23,17 @@ for key, val in weather_dict.items():
     detailed_weather = info["description"]
     is_umbrella = "Bring an Umbrella!" if int(code) < 700 else "Dont Need Umbrella!"
 
-    print(f"{time=}, {code=}, {weather=}, {detailed_weather=}, {is_umbrella}")
+    weather_information.append(f"{time=}, {code=}, {weather=}, {detailed_weather=}, {is_umbrella}")
+
+message_content = "\n\n".join(weather_information)
+account_sid = "AC02e03f4957ae0746b325950da2aad98c"
+auth_token = "925f0ac139caa08c93fad7bbf3c6243a"
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    body=message_content,
+    from_="whatsapp:+14155238886",
+    to="whatsapp:+447547273613",
+)
+
+print(message.status)
